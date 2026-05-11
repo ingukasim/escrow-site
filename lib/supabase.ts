ba1e5dc90cd65fe47c4fbca7feaf81ae7d3a8429
Muhamad Kassim <you@example.com>
2026-05-12 01:55:24 +0800

    Added fallback Supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ftzwloyrezghdavmdvns.supabase.co";
+
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0endsb3lyZXpnaGRhdm1kdm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MDQ4ODQsImV4cCI6MjA5Mzk4MDg4NH0.9YIDLDhY605HVsE9QM5id-n77eqDQ2i6JbCRVtPhPhE";
 
 export const supabase = createClient(
   supabaseUrl,
