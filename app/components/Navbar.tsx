ffbe9c8f3aa8db6087b0184767ccf7b254fd6608
Muhamad Kassim <you@example.com>
2026-05-17 00:25:19 +0800

    Hide logout button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -52,14 +52,14 @@ export default function Navbar() {
 
           </Link>
 
-          <button
-            onClick={handleLogout}
-            className="bg-red-500 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-2xl transition-all"
-          >
-
-            Logout
-
-          </button>
+       {false && (
+  <button
+    onClick={handleLogout}
+    className="bg-red-500 hover:bg-red-400 text-white font-bold px-5 py-2 rounded-2xl transition-all"
+  >
+    Logout
+  </button>
+)}
 
         </div>
 
